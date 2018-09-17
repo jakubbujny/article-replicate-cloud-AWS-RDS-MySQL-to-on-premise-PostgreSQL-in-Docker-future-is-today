e66ad692cf06471921ef9485cac01b96d1f927ea
Jakub Bujny <jakub.bujny@ac-project.net>
2018-09-17 23:05:20 +0200

    Final, working solution

--- sql-scripts/rds.sql
+++ sql-scripts/rds.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS my_debezium_test (
   PRIMARY KEY (ID)
 );
 
-gINSERT INTO my_debezium_test VALUES(3, "it rox!!");
+INSERT INTO my_debezium_test VALUES(3, "it rox!!");
 
 UPDATE my_debezium_test SET test = "my friend!" WHERE ID = 2;
 
